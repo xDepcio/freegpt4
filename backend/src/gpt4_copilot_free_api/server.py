7dbcf274e5d9fff12f0a1cc019a30a1ddc2f9922
xDepcio <olek.drwal@gmail.com>
2024-04-17 16:42:54 +0200

    sutup vite with flask

--- backend/src/gpt4_copilot_free_api/server.py
+++ backend/src/gpt4_copilot_free_api/server.py
@@ -1,24 +1,37 @@
-from flask import Flask, send_from_directory
+from flask import Flask, render_template, send_from_directory
 
-app = Flask(__name__, static_folder="build")
-
-
-@app.route("/")
-def serve():
-    """Serves the React app."""
-    return send_from_directory(app.static_folder, "index.html")
+app = Flask(
+    __name__,
+    static_folder="../../../frontend/dist/static",
+    template_folder="../../../frontend/dist",
+)
 
 
+@app.route("/", defaults={"path": ""})
 @app.route("/<path:path>")
-def serve_static(path):
-    """Serves static files."""
-    if path.endswith(".js"):
-        mimetype = "application/javascript"
-    elif path.endswith(".css"):
-        mimetype = "text/css"
-    else:
-        mimetype = "text/html"
-    return send_from_directory(app.static_folder, path, mimetype=mimetype)
+def catch_all(path):
+    print(app.static_folder)
+    print(app.template_folder)
+    return render_template("index.html")
+
+
+# @app.route("/")
+# def serve():
+#     """Serves the React app."""
+#     print(app.static_folder)
+#     return send_from_directory(app.static_folder, "index.html")
+
+
+# @app.route("/assets/<path:path>")
+# def serve_static(path):
+#     """Serves static files."""
+#     if path.endswith(".js"):
+#         mimetype = "application/javascript"
+#     elif path.endswith(".css"):
+#         mimetype = "text/css"
+#     else:
+#         mimetype = "text/html"
+#     return send_from_directory(app.static_folder, path, mimetype=mimetype)
 
 
 if __name__ == "__main__":
