e5a53893167a3cc9ed20caba85561ae50141a2be
xDepcio <olek.drwal@gmail.com>
2024-04-17 16:46:30 +0200

    vite template with tailwind and shadcn

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    assetsDir: 'static',
-  }
-})
+    plugins: [react()],
+    resolve: {
+        alias: {
+            '@': resolve(__dirname, './src'),
+        },
+    },
+    build: {
+        assetsDir: 'static',
+    }
+});
